7d16f7cc2852ccf49a09aa38772da3a4c1fe0444
adrianbparra <adrianplane12@gmail.com>
2019-12-07 13:01:23 -0700

    reafactored code and added the save option. Need to find a way to add save button on the movielist componenet as well it only works in the movie component.

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,9 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
-      <Route path='/movies/:id' component={Movie} />
+      <Route path='/movies/:id' render={routeProps => {
+        return <Movie addToSavedList={addToSavedList} />
+      }} />
       
     </div>
   );
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-const MovieCard = props => {
-  return;
-};
+//do a find like the example on codesandbox
+
+
+ 
+function MovieCard({ movie, saveMovie }) {
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className= "movie-card">
+      <Link to={`/movies/${movie.id}`}>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </Link>
+      <div className="save-button" onClick={saveMovie}>Save</div>
+    </div>
+
+  );
+}
 
 export default MovieCard;
